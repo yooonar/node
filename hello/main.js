169ed041811d443eb1b3266a011e9deadf478865
yooonar <aryooon33@gmail.com>
2020-12-17 07:55:47 +0900

    main page

--- hello/main.js
+++ hello/main.js
@@ -6,12 +6,35 @@ var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url,true).pathname; // pathname : 쿼리스트링 제외 path : 쿼리스트링 포함
-    var title = queryData.id;
     console.log(queryData.id);
 
     if(pathname === '/') {
-        fs.readFile(`../data/${queryData.id}`, 'utf8', function(err, description) {
+        if(queryData.id === undefined) { // HOME
+            var title = 'Welcome';
+            var description = 'Hello, Node.js';
             var template = `<!doctype html>
+            <html>
+            <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${description}</p>
+            </body>
+            </html>`;
+            response.writeHead(200);
+            response.end(template);
+        } else {
+            fs.readFile(`../data/${queryData.id}`, 'utf8', function(err, description) {
+                var title = queryData.id;
+                var template = `<!doctype html>
                 <html>
                 <head>
                     <title>WEB1 - ${title}</title>
@@ -28,9 +51,10 @@ var app = http.createServer(function(request, response) {
                 <p>${description}</p>
                 </body>
                 </html>`;
-            response.writeHead(200);
-            response.end(template);
-        });
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
     } else {
         response.writeHead(404);
         response.end('Not Found');
